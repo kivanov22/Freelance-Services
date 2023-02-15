3a134bcad8b62c82633b152a62b0052b1bb7bd20
Kristian Ivanov <kriskopk@abv.bg>
2023-02-15 17:29:17 +0200

    Created empty pages with theirs styles.Created navbar component with logic to change color on scroll.

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [active, setActive] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive);
+
+    return () => {
+      window.removeEventListener("scroll", isActive);
+    };
+  }, []);
+
+  const currentUser = {
+    id: 1,
+    username: "Kristian Ivanov",
+    isSeller: true,
+  };
+
+  return (
+    <div className={active ? "navbar active" : "navbar"}>
+      <div className="container">
+        <div className="logo">
+          {/* <Link to="/"> */}
+          <span className="text">fiverr</span>
+          {/* </Link> */}
+          <span className="dot">.</span>
+        </div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser && <button>Join</button>}
+          {currentUser && (
+            <div className="user" onClick={() => setOpen(!open)}>
+              <img
+                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>{currentUser?.username}</span>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Test</span>
+            <span>Test2</span>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
