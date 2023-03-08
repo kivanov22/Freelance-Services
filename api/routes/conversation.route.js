ae60ff49b8b09d2b7af2284d643e4229326efe8d
Kristian Ivanov <kriskopk@abv.bg>
2023-03-08 22:08:09 +0200

    Conversation router and controller.

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,6 +1,17 @@
 import express from "express"
+import {
+    getConversations,
+    createConversation,
+    getSingleConversation,
+    updateConversation
+} from "../controllers/conversation.controller.js";
+import veirfyToken from "../middlewares/jwt.js";
 
 const router = express.Router()
 
+router.get("/",veirfyToken,getConversations);
+router.post("/",veirfyToken,createConversation);
+router.get("/single/:id",veirfyToken,getSingleConversation);
+router.post("/:id",veirfyToken,updateConversation);
 
 export default router;
